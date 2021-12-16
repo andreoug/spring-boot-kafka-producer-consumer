81f1ae3d1a8896f0b0befd22cc153470d4fe3b07
George Andreou <allemaos@gmail.com>
2021-12-16 14:39:14 +0200

    minor changes to be aligned with start.spring.io output

--- src/test/java/com/pilot/springbootkafkaproducerconsumer/SpringBootKafkaProducerConsumerApplicationTests.java
+++ src/test/java/com/pilot/springbootkafkaproducerconsumer/SpringBootKafkaProducerConsumerApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SpringBootKafkaProducerConsumerApplicationTests {
+class SpringBootKafkaProducerConsumerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
